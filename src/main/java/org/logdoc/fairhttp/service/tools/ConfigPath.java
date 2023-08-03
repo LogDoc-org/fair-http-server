20e1f3d6f63b9c3f4b9b315324285fd31999ebfd
lucky <me@loslobos.ru>
2023-08-03 19:08:05 +0300

    typo fix

--- src/main/java/org/logdoc/fairhttp/service/tools/ConfigPath.java
+++ src/main/java/org/logdoc/fairhttp/service/tools/ConfigPath.java
@@ -8,7 +8,7 @@ package org.logdoc.fairhttp.service.tools;
 public interface ConfigPath {
     String PORT = "fair.http.port";
     String MAX_REQUEST = "fair.http.max_request_body";
-    String READ_TIMEOUT = "fair.http.request_read_timeout";
+    String READ_TIMEOUT = "fair.http.request_read_timeout_ms";
     String CORS = "fair.http.cors";
     String CORS_ORIGINS = "origins";
     String CORS_METHODS = "methods";
