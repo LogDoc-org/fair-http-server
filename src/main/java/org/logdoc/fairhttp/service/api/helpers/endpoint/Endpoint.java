d88d68224457ca8821fd2100d16c63b152731eec
lucky <me@loslobos.ru>
2024-02-29 14:03:24 +0300

    Removed aop

--- src/main/java/org/logdoc/fairhttp/service/api/helpers/endpoint/Endpoint.java
+++ src/main/java/org/logdoc/fairhttp/service/api/helpers/endpoint/Endpoint.java
@@ -1,9 +1,5 @@
 package org.logdoc.fairhttp.service.api.helpers.endpoint;
 
-import org.logdoc.fairhttp.service.api.helpers.aop.Post;
-import org.logdoc.fairhttp.service.api.helpers.aop.PostChain;
-import org.logdoc.fairhttp.service.api.helpers.aop.Pre;
-import org.logdoc.fairhttp.service.api.helpers.aop.PreChain;
 import org.logdoc.fairhttp.service.http.Request;
 import org.logdoc.fairhttp.service.http.Response;
 import org.logdoc.helpers.gears.Pair;
@@ -25,16 +21,12 @@ public class Endpoint implements Comparable<Endpoint> {
     private final String method;
     private final Signature signature;
     private final BiFunction<Request, Map<String, String>, Response> invoker;
-    private final PreChain pre;
-    private final PostChain post;
 
     public Endpoint(final String method, final Signature signature, final BiFunction<Request, Map<String, String>, Response> invoker) {
         this.method = method;
         this.signature = signature;
         this.invoker = invoker;
 
-        pre = new PreChain();
-        post = new PostChain();
     }
 
     public Pair<Boolean, Boolean> match(final String method, final String hardPath) {
@@ -42,31 +34,8 @@ public class Endpoint implements Comparable<Endpoint> {
     }
 
     public Response call(final Request request) {
-        final Response broken = pre.apply(request);
-
-        if (broken != null) {
-            preLogger.warn(this + " :: request call interrupted by pre-filters");
-            return broken;
-        }
-
-        return post.apply(request, invoker.apply(request, signature.values(request.path())));
+        return invoker.apply(request, signature.values(request.path()));
     }
-
-    public void removePre(final Pre pre) {
-        this.pre.remove(pre);
-    }
-
-    public void removePost(final Post post) {
-        this.post.remove(post);
-    }
-
-    public void addFirstPre(final Pre pre) {this.pre.addFirst(pre);}
-
-    public void addLastPre(final Pre pre) {this.pre.addLast(pre);}
-
-    public void addFirstPost(final Post pre) {post.addFirst(pre);}
-
-    public void addLastPost(final Post pre) {post.addLast(pre);}
 
     @Override
     public int compareTo(final Endpoint o) {
